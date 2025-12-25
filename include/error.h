f6c5a0daef9cf122dd60d1d1603453c6a1789acb
Ali Hamza <ali.mhamza06@gmail.com>
2025-12-25 20:17:53 +0400

    Change exit code to match Bash's.

--- include/error.h
+++ include/error.h
@@ -4,9 +4,9 @@
 #include "config.h"
 
 #define GEN_ERROR       1       // Generic error exit code.
+#define INVALID_EXIT    2       // Exit code format is invalid (e.g., decimal number or containing letters).
 #define NOT_EXEC        126     // File given is not executable (lack of permissions, wrong file type, etc.).
 #define NOT_FOUND       127     // Executable given was not found.
-#define INVALID_EXIT    128     // Exit code format is invalid (e.g., decimal number or containing letters).
 #define EXIT_RANGE      255     // Exit code is outside of valid range 0-255 (i.e., negative or >255).
 
 static inline void setConfigExitCode(sConfig* conf, unsigned char exitCode)
