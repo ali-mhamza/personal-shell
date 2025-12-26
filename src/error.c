1aff3d7ad0ed0e8f4fb354e40172a045b26ea4ff
Ali Hamza <ali.mhamza06@gmail.com>
2025-12-26 06:10:32 +0400

    Replace prohibited external function with personal code.
    This is a very slightly modified version of my own ft_printf, made to accept a user-chosen file descriptor and an already-initialized va_list object instead of its regular parameters.

--- src/error.c
+++ src/error.c
@@ -4,13 +4,15 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int ft_vfprintf(int fd, const char *format, va_list *ap);
+
 void reportError(const char* type, const char* format, ...)
 {
     write(2, type, strlen(type));
     write(2, ": ", 2);
     va_list ap;
     va_start(ap, format);
-    vfprintf(stderr, format, ap);
+    ft_vfprintf(STDERR_FILENO, format, &ap);
     write(2, "\n", 1);
     va_end(ap);
 }
